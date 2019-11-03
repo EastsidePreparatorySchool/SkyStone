60c2b0450b96128cea371cab62e8124e74f83d43
TEspelien <tespelien@eastsideprep.org>
2019-11-02 22:27:58 -0700

    just have to change coeffcitints in testng

--- TeamCode/src/main/java/org/eastsideprep/eps8103/basicSkyStoneAuto.java
+++ TeamCode/src/main/java/org/eastsideprep/eps8103/basicSkyStoneAuto.java
@@ -20,114 +20,122 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import java.util.ArrayList;
 import java.util.List;
 
-@Autonomous(name = "leftpark", group = "Concept")
+@Autonomous(name = "dead reckoning", group = "Concept")
 public class basicSkyStoneAuto extends LinearOpMode {
 
     Hardware8103 robot = new Hardware8103();
 
-    public static final String TAG = "Vuforia Navigation Sample";
+    //these have to be found experimentally aka a lot of testing
+    int fast_c = 1;
+    int strafe_c = 1;
+    int slow_strafe_c = 1;
+    int ext_c = 1;
+    int angle_c = 1;
 
     public void forwards(int l) {
         for(DcMotor m: robot.allMotors) {
-            m.setTargetPosition(l);
-            m.setPower(0.1);
+            m.setTargetPosition(fast_c*l);
+            m.setPower(0.5);
             //sleep(l * 48);
         }
     }
 
     public void backwards(int l) {
         for(DcMotor m: robot.allMotors) {
-            m.setTargetPosition(-1*l);
-            m.setPower(0.1);
+            m.setTargetPosition(fast_c*l);
+            m.setPower(0.5);
             //sleep(l * 48);
         }
     }
 
-    public void turnright() {
+    public void turnright(int angle) {
         robot.leftBackMotor.setPower(-0.25); //have some wheels turn different directions so it goes left
         robot.leftFrontMotor.setPower(0.25);
         robot.rightBackMotor.setPower(0.25);
         robot.rightFrontMotor.setPower(-0.25);
+        sleep(turn_c * angle);
     }
 
-    public void turnrightslowly() {
+    public void turnrightslowly(int angle) {
         robot.leftBackMotor.setPower(-0.05); //have low power so it goes very slow
         robot.leftFrontMotor.setPower(0.05);
         robot.rightFrontMotor.setPower(0.05);
         robot.rightBackMotor.setPower(-0.05);
+        sleep(turn_c*angle*5)
     }
 
-    public void turnleft() {
+    public void turnleft(int angle) {
         robot.leftFrontMotor.setPower(0.25);
         robot.leftBackMotor.setPower(-0.25);
         robot.rightBackMotor.setPower(-0.25);
         robot.rightFrontMotor.setPower(0.25);
-    }
-
-    public void motorStop() {
-        robot.rightFrontMotor.setPower(0);
-        robot.rightBackMotor.setPower(0);
-        robot.leftFrontMotor.setPower(0);
-        robot.leftBackMotor.setPower(0);
+        sleep(turn_c * angle);
     }
 
     public void stopmotors() {
-        robot.rightBackMotor.setPower(0);
-        robot.rightFrontMotor.setPower(0);
-        robot.leftBackMotor.setPower(0);
-        robot.leftFrontMotor.setPower(0);
-    }
-
-    public void strafeleftslowly() {
-        robot.rightFrontMotor.setPower(0.05);
-        robot.rightBackMotor.setPower(-0.05);
-        robot.leftFrontMotor.setPower(-0.05);
-        robot.leftBackMotor.setPower(0.05);
-    }
-
-    public void straferightslowly() {
-        robot.rightFrontMotor.setPower(-0.05);
-        robot.rightBackMotor.setPower(0.05);
-        robot.leftFrontMotor.setPower(0.05);
-        robot.leftBackMotor.setPower(-0.05);
-        //sleep(l*12*2000);
-    }
-
-    public void strafeleft(int l) {
-        robot.rightFrontMotor.setTargetPosition(l);
-        robot.rightBackMotor.setTargetPosition(-1*l);
-        robot.leftFrontMotor.setTargetPosition(-1*l);
-        robot.leftBackMotor.setTargetPosition(l);
-        for(DcMotor m: robot.allMotors){
-            m.setPower(0.01);
+        for (DcMotor m: robot.allMotors){
+            m.setPower(0);
         }
     }
 
-    public void straferight() {
-        robot.rightFrontMotor.setPower(-0.5);
-        robot.rightBackMotor.setPower(0.5);
-        robot.leftFrontMotor.setPower(0.5);
-        robot.leftBackMotor.setPower(-0.5);
+    public void strafeleftslowly() {
+        robot.rightFrontMotor.setTargetPosition(slow_strafe_c * l);
+        robot.rightBackMotor.setTargetPosition(-1*slow_strafe_c * l);
+        robot.leftFrontMotor.setTargetPosition(-1*slow_strafe_c * l);
+        robot.leftBackMotor.setTargetPosition(slow_strafe_c * l);
+        for(DcMotor m: robot.allMotors){
+            m.setPower(0.1);
+        }
+    }
+
+    public void straferightslowly() {
+        robot.rightFrontMotor.setTargetPosition(-1 * slow_strafe_c * l);
+        robot.rightBackMotor.setTargetPosition(slow_strafe_c * l);
+        robot.leftFrontMotor.setTargetPosition(slow_strafe_c * l);
+        robot.leftBackMotor.setTargetPosition(-1 * slow_strafe_c * l);
+        for(DcMotor m: robot.allMotors){
+            m.setPower(0.1);
+        }
+    }
+
+    public void strafeleft(int l) {
+        robot.rightFrontMotor.setTargetPosition(strafe_c * l);
+        robot.rightBackMotor.setTargetPosition(-1*strafe_c * l);
+        robot.leftFrontMotor.setTargetPosition(-1*strafe_c * l);
+        robot.leftBackMotor.setTargetPosition(strafe_c * l);
+        for(DcMotor m: robot.allMotors){
+            m.setPower(0.25);
+        }
+    }
+
+    public void straferight(int l) {
+        robot.rightFrontMotor.setTargetPosition(-1 * strafe_c * l);
+        robot.rightBackMotor.setTargetPosition(strafe_c * l);
+        robot.leftFrontMotor.setTargetPosition(strafe_c * l);
+        robot.leftBackMotor.setTargetPosition(-1 * strafe_c * l);
+        for(DcMotor m: robot.allMotors){
+            m.setPower(0.25);
+        }
     }
 
     public void lowerarm(int angle) {
-        robot.armPivot.setPower(0.6);
-        sleep(1000 * angle);
+        robot.armPivot.setTargetPosition(angle_c *angle);
+        robot.armPivot.setPower(1);
     }
 
     public void raisearm(int angle) {
-        robot.armPivot.setPower(-1);
-        sleep(1000 * angle);
+        robot.armPivot.setTargetPosition(-1 * angle_c * angle);
+        robot.armPivot.setPower(1);
     }
 
     public void extendarm(int l) {
-        robot.armExtender.setPower(-1);
-        sleep(1000 * l);
+        robot.armExtender.setTargetPosition(ext_c * l);
+        robot.armExtender.setPower(1);
     }
 
     public void reelarm(int l) {
+        robot.armExtender.setTargetPosition(-1 * ext_c * l);
         robot.armExtender.setPower(1);
-        sleep(1000 * l);
     }
 
 
@@ -151,8 +159,21 @@ public class basicSkyStoneAuto extends LinearOpMode {
 //            telemetry.addData("log", "went forwards");
 //            backwards(24);
 //            telemetry.addData("log", "went backwards");
-            strafeleft(9);
-            telemetry.addData("log", "strafed");
+            forwards(24);
+            backwards(24);
+            telemetry.addData("for/bac", "24, 24");
+
+            straferight(12);
+            strafeleft(12);
+            telemetry.addData("R/L", "12, 12");
+
+            turnrightslowly(45);
+            turnright(45);
+            turnleft(90);
+            telemetry.addData("turning", "45R, 45R, 90L");
+            
+
+            telemetry.update();
         }
     }
 }
